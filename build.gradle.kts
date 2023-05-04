621a3c6bdc36161a9a0a3c204a8658dc71ca718c
Arewena <sjwj08@gmail.com>
2023-05-04 11:18:29 +0900

    BuildException 에러 또또또 수정 / 드디어 알맞은 라이브러리 import(이제부터 열심히 작업하는걸로) (5/4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
-
 plugins {
     kotlin("jvm") version "1.8.0"
 }
@@ -9,16 +7,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
     maven("https://papermc.io/repo/repository/maven-public/")
-    maven("https://jitpack.io")
+    maven("https://jitpack.io/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:5.0.0-beta.1")
+    implementation("com.github.minndevelopment:jda-ktx:34b55c0")
     implementation("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
-    implementation("net.dv8tion:JDA:3.0.0_156")
-    implementation("com.github.JDA-Applications:Kotlin-JDA:master-SNAPSHOT")
 
 }
 
--- src/main/kotlin/io/github/arewena/Main.kt
+++ src/main/kotlin/io/github/arewena/Main.kt
@@ -1,11 +1,21 @@
 package io.github.arewena
 
+
+import dev.minn.jda.ktx.intents
+import dev.minn.jda.ktx.light
+import net.dv8tion.jda.api.requests.GatewayIntent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerChatEvent
 import org.bukkit.plugin.java.JavaPlugin
 
+
+
 class Main : JavaPlugin(), Listener {
+    val jda = light("token", enableCoroutines = true) {
+        intents += listOf(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT)
+    }
+
     override fun onEnable() {
         logger.info("Plugin Enabled")
         this.server.pluginManager.registerEvents(this, this)
