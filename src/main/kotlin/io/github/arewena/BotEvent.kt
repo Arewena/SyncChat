345f4ec70266ea5930d0498b16a5b95a78333d02
Arewena <sjwj08@gmail.com>
2023-05-06 22:45:02 +0900

    틀 잡기 (5/6)

--- src/main/kotlin/io/github/arewena/BotEvent.kt
+++ src/main/kotlin/io/github/arewena/BotEvent.kt
@@ -1,7 +1,17 @@
 package io.github.arewena
 
+import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.hooks.EventListener
 
-abstract class BotEvent : EventListener {
+
+class BotEvent : EventListener {
+    // TODO: 슬래시 커맨드로 메시지 보낼 채널 설정 여부 묻는 커맨드 만들기, 메시지 보내는 함수 하나 만들기
+
+
+
+    override fun onEvent(event: GenericEvent) {
+        TODO("Not yet implemented")
+    }
+
 
 }
--- src/main/kotlin/io/github/arewena/MinecraftEvent.kt
+++ src/main/kotlin/io/github/arewena/MinecraftEvent.kt
@@ -1,11 +1,23 @@
 package io.github.arewena
 
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerChatEvent
 
-class MinecraftEvent : Listener {
-    @EventHandler
-    fun onChat(event: PlayerChatEvent) {
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+
+
+class MinecraftEvent : JavaPlugin(), Listener {
+    // TODO: 메시지 입력 감지, 디코에서 온 메시지 보내는 함수 만들기
+
+
+    override fun onEnable() {
+        val jda = JDABuilder.createDefault("OTE1MTQyNzEyNDAyMDAxOTIw.GHkucr.do5Nmyak2CFL2tBvtVrFaUB0D_XdloklkB3E7Y").build()
+        jda.presence.setStatus(OnlineStatus.ONLINE)
+        logger.info("Plugin Enabled")
+        this.server.pluginManager.registerEvents(this, this)
     }
+
+
 }
