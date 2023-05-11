6e1dd9ae03e2e4adbd7c51adbbe9d68325337da9
Arewena <sjwj08@gmail.com>
2023-05-11 11:28:28 +0900

    selected-channel 명령어 구현

--- src/main/kotlin/io/github/arewena/BotEvent.kt
+++ src/main/kotlin/io/github/arewena/BotEvent.kt
@@ -7,10 +7,12 @@ import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
+var selctedChannel: String? = ""
 
 class BotEvent : EventListener, ListenerAdapter() {
-    // TODO: 슬래시 커맨드로 메시지 보낼 채널 설정 여부 묻는 커맨드 만들기, 메시지 보내는 함수 하나 만들기
-    val selctedChannel = ""
+    // TODO: 메시지 보내는 함수 하나 만들기, selectedChannel 변수 어떻게 써먹을지 구상하기
+
+
 
     fun set(token: String) {
         val jda = JDABuilder.createLight(token)
@@ -19,12 +21,24 @@ class BotEvent : EventListener, ListenerAdapter() {
             .build()
 
         jda.updateCommands().addCommands(
-            Commands.slash("asdf", "asdf")
+            Commands.slash("select-channel", "Minecraft 메시지를 출력할 채널을 설정하는 명령어 입니다.")
         ).queue()
 
     }
 
+
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        if (event.member?.isOwner!!) {
+            val name = event.name
+            val channelName = event.channel.name
+            when(name) {
+                "select-channel" -> event.reply("Minecraft 인게임 채팅과 $channelName 채널이 성공적으로 연결되었습니다.").setEphemeral(true).queue()
+
+            }
+        }
+        else {
+            event.reply("이 명령어를 사용할 권한이 없어요.").setEphemeral(true).queue()
+        }
 
 
     }
--- src/main/kotlin/io/github/arewena/MinecraftEvent.kt
+++ src/main/kotlin/io/github/arewena/MinecraftEvent.kt
@@ -7,11 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin
 
 
 class MinecraftEvent : JavaPlugin(), Listener {
-    // TODO: 메시지 입력 감지, 디코에서 온 메시지 보내는 함수 만들기
+    // TODO: 메시지 입력 감지, 디코에서 온 메시지 보내는 함수 만들기, shutdown 어떻게 할지 구상하기
+    // 토큰 지우는거 잊어먹지 말기!!
 
 
     override fun onEnable() {
-        val start = BotEvent().set("")
+        val start = BotEvent().set("token")
         logger.info("Plugin Enabled")
         this.server.pluginManager.registerEvents(this, this)
     }
