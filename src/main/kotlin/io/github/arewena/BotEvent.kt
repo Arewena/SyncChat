ac266764b8b76fa1e643450840d5655eefe8555a
Arewena <sjwj08@gmail.com>
2023-05-12 13:51:55 +0900

    마인크래프트 -> 디스코드 채팅 보내는 기능 구현 (5/12)

--- src/main/kotlin/io/github/arewena/BotEvent.kt
+++ src/main/kotlin/io/github/arewena/BotEvent.kt
@@ -2,16 +2,16 @@ package io.github.arewena
 
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
-var selctedChannel: String? = ""
 
 class BotEvent : EventListener, ListenerAdapter() {
-    // TODO: 메시지 보내는 함수 하나 만들기, selectedChannel 변수 어떻게 써먹을지 구상하기
-
+    // TODO: 디스코드 -> 마인크래프트로 채팅을 보낼 방법 구상하기, 서버 입/퇴장 메시지, 사망 메시지, 발전과제 달성 메시지 임베드화
+    lateinit var selectedChannel: MessageChannelUnion
 
 
     fun set(token: String) {
@@ -21,8 +21,19 @@ class BotEvent : EventListener, ListenerAdapter() {
             .build()
 
         jda.updateCommands().addCommands(
-            Commands.slash("select-channel", "Minecraft 메시지를 출력할 채널을 설정하는 명령어 입니다.")
+            Commands.slash("select-channel", "Minecraft 메시지를 출력할 채널을 설정하는 명령어 입니다."),
         ).queue()
+
+    }
+
+    fun message(user: String, message: String, type: Int) {
+        when (type) {
+            1 -> selectedChannel.sendMessage("<$user> $message")
+            2 -> selectedChannel.sendMessage("$user (이)가 서버에 입장하셨습나다.")
+            3 -> selectedChannel.sendMessage("$user (이)가 서버에서 퇴장하셨습니다.")
+            4 -> selectedChannel.sendMessage("$user (이)가 사망혔습니다!")
+            5 -> selectedChannel.sendMessage("$user (이)가 $message 발전과제를 달성하셨습니다!")
+        }
 
     }
 
@@ -30,21 +41,19 @@ class BotEvent : EventListener, ListenerAdapter() {
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         if (event.member?.isOwner!!) {
             val name = event.name
-            val channelName = event.channel.name
+            val channelName = "https://discord.com/channels/${event.guild?.id}/${event.channel.id}"
             when(name) {
-                "select-channel" -> event.reply("Minecraft 인게임 채팅과 $channelName 채널이 성공적으로 연결되었습니다.").setEphemeral(true).queue()
+                "select-channel" -> {
+                    event.reply("Minecraft 인게임 채팅과 $channelName 채널이 성공적으로 연결되었습니다.").setEphemeral(true).queue()
+                    selectedChannel = event.channel
+                }
+
 
             }
+
         }
         else {
             event.reply("이 명령어를 사용할 권한이 없어요.").setEphemeral(true).queue()
         }
-
-
     }
-
-
-
-
-
 }
--- src/main/kotlin/io/github/arewena/MinecraftEvent.kt
+++ src/main/kotlin/io/github/arewena/MinecraftEvent.kt
@@ -1,21 +1,42 @@
 package io.github.arewena
 
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerAdvancementDoneEvent
+import org.bukkit.event.player.PlayerChatEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 
 
 class MinecraftEvent : JavaPlugin(), Listener {
-    // TODO: 메시지 입력 감지, 디코에서 온 메시지 보내는 함수 만들기, shutdown 어떻게 할지 구상하기
+    // TODO: 디스코드 -> 마인크래프트로 채팅 보내는 함수 만들기, shutdown 어떻게 할지 구상하기, 발전과제 달성 메시지랑 데스 메시지 번역(가능하면)
     // 토큰 지우는거 잊어먹지 말기!!
 
 
     override fun onEnable() {
-        val start = BotEvent().set("token")
+        val start = BotEvent().set("")
         logger.info("Plugin Enabled")
         this.server.pluginManager.registerEvents(this, this)
     }
+
+    @EventHandler
+    fun onChat(event: PlayerChatEvent) { BotEvent().message(event.player.name, event.message, 1) }
+
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) { BotEvent().message(event.player.name, "", 2) }
+
+    @EventHandler
+    fun onLeft(event: PlayerQuitEvent) { BotEvent().message(event.player.name, "", 3) }
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {BotEvent().message(event.player.name, "", 4)}
+
+    @EventHandler
+    fun onDone(event: PlayerAdvancementDoneEvent) {BotEvent().message(event.player.name, event.eventName, 5)}
 
 
 }
