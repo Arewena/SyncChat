958ca4d8e20e790aa6e4906199cb3c3da5786867
Arewena <sjwj08@gmail.com>
2023-05-20 10:55:43 +0900

    대대적인 버그 수정 (5/20)

--- src/main/kotlin/io/github/arewena/BotEvent.kt
+++ src/main/kotlin/io/github/arewena/BotEvent.kt
@@ -1,40 +1,52 @@
 package io.github.arewena
 
+import dev.minn.jda.ktx.EmbedBuilder
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.OnlineStatus
-import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
 
+val jda = JDABuilder.createLight("token")
+    .addEventListeners(BotEvent())
+    .setStatus(OnlineStatus.ONLINE)
+    .build()
+
+var selectedChannel: Long? = null
+
+
+
 class BotEvent : EventListener, ListenerAdapter() {
-    // TODO: 디스코드 -> 마인크래프트로 채팅을 보낼 방법 구상하기, 서버 입/퇴장 메시지, 사망 메시지, 발전과제 달성 메시지 임베드화
-    lateinit var selectedChannel: MessageChannelUnion
+    // TODO: 디스코드 -> 마인크래프트로 채팅을 보낼 방법 구상하기, 임베드 수정
 
-
-    fun set(token: String) {
-        val jda = JDABuilder.createLight(token)
-            .addEventListeners(BotEvent())
-            .setStatus(OnlineStatus.ONLINE)
-            .build()
-
+    fun set() {
         jda.updateCommands().addCommands(
             Commands.slash("select-channel", "Minecraft 메시지를 출력할 채널을 설정하는 명령어 입니다."),
-        ).queue()
-
+            Commands.slash("info", ".")).queue()
     }
 
     fun message(user: String, message: String, type: Int) {
-        when (type) {
-            1 -> selectedChannel.sendMessage("<$user> $message")
-            2 -> selectedChannel.sendMessage("$user (이)가 서버에 입장하셨습나다.")
-            3 -> selectedChannel.sendMessage("$user (이)가 서버에서 퇴장하셨습니다.")
-            4 -> selectedChannel.sendMessage("$user (이)가 사망혔습니다!")
-            5 -> selectedChannel.sendMessage("$user (이)가 $message 발전과제를 달성하셨습니다!")
-        }
 
+        if (selectedChannel != null) {
+            val selectedChannel = selectedChannel
+            when (type) {
+                1 -> jda.getTextChannelById(selectedChannel!!)?.sendMessage("<$user> $message")?.queue()
+                2 -> jda.getTextChannelById(selectedChannel!!)?.sendMessageEmbeds(EmbedBuilder().apply {
+                    author("$user (이)가 서버에 입장하셨습니다.", null, null)
+                }.build())?.queue()
+                3 -> jda.getTextChannelById(selectedChannel!!)?.sendMessageEmbeds(EmbedBuilder().apply {
+                    author("$user (이)가 서버에서 퇴장하셨습니다.", null, null)
+                }.build())?.queue()
+                4 -> jda.getTextChannelById(selectedChannel!!)?.sendMessageEmbeds(EmbedBuilder().apply {
+                    author("$user (이)가 사망하셨습니다!", null, null)
+                }.build())?.queue()
+                5 -> jda.getTextChannelById(selectedChannel!!)?.sendMessageEmbeds(EmbedBuilder().apply {
+                    author("$user (이)가 $message 발전과제를 달성하셨습니다!", null, null)
+                }.build())?.queue()
+            }
+        }
     }
 
 
@@ -44,16 +56,20 @@ class BotEvent : EventListener, ListenerAdapter() {
             val channelName = "https://discord.com/channels/${event.guild?.id}/${event.channel.id}"
             when(name) {
                 "select-channel" -> {
+                    selectedChannel = event.channel.idLong
                     event.reply("Minecraft 인게임 채팅과 $channelName 채널이 성공적으로 연결되었습니다.").setEphemeral(true).queue()
-                    selectedChannel = event.channel
                 }
+                "info" -> {
+                    event.reply(selectedChannel.toString()).queue()
+                }
+
 
 
             }
 
         }
         else {
-            event.reply("이 명령어를 사용할 권한이 없어요.").setEphemeral(true).queue()
+            event.reply("이 명령어를 사용할 권한이 없습니다.").setEphemeral(true).queue()
         }
     }
 }
--- src/main/kotlin/io/github/arewena/MinecraftEvent.kt
+++ src/main/kotlin/io/github/arewena/MinecraftEvent.kt
@@ -13,18 +13,23 @@ import org.bukkit.plugin.java.JavaPlugin
 
 
 class MinecraftEvent : JavaPlugin(), Listener {
-    // TODO: 디스코드 -> 마인크래프트로 채팅 보내는 함수 만들기, shutdown 어떻게 할지 구상하기, 발전과제 달성 메시지랑 데스 메시지 번역(가능하면)
+    // TODO: 디스코드 -> 마인크래프트로 채팅 보내는 함수 만들기, shutdown 어떻게 할지 구상하기, 발전과제 메시지 수정
     // 토큰 지우는거 잊어먹지 말기!!
 
 
     override fun onEnable() {
-        val start = BotEvent().set("")
         logger.info("Plugin Enabled")
+        val jda = jda
+        BotEvent().set()
         this.server.pluginManager.registerEvents(this, this)
+
     }
 
     @EventHandler
-    fun onChat(event: PlayerChatEvent) { BotEvent().message(event.player.name, event.message, 1) }
+    fun onChat(event: PlayerChatEvent) {
+        logger.info(selectedChannel.toString())
+        BotEvent().message(event.player.name, event.message, 1)
+    }
 
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) { BotEvent().message(event.player.name, "", 2) }
@@ -36,7 +41,7 @@ class MinecraftEvent : JavaPlugin(), Listener {
     fun onDeath(event: PlayerDeathEvent) {BotEvent().message(event.player.name, "", 4)}
 
     @EventHandler
-    fun onDone(event: PlayerAdvancementDoneEvent) {BotEvent().message(event.player.name, event.eventName, 5)}
+    fun onDone(event: PlayerAdvancementDoneEvent) {BotEvent().message(event.player.name, event.advancement.toString(), 5)}
 
 
 }
